7d986512f81e459931991e39d357968fdd136269
adrien Bodineau <adrien.bodineau@gmail.com>
2016-04-07 17:57:24 +0200

    add gitignore and implement basic 2048 game

---
+++ python/2048.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python3
+# -*- encoding: utf-8 -*-
+
+from random import randint
+HAUT = 'HAUT'
+BAS = 'BAS'
+GAUCHE = 'GAUCHE'
+DROITE = 'DROITE'
+
+class Game():
+    def __init__(self):
+        self.mat = [[0,0,0,0],[0,0,0,0],[0,0,0,0], [0,0,0,0]]
+        self.free = [(i, j) for i in range(4) for j in range(4) if not self.mat[i][j]]
+        x,y = self.free[randint(0, len(self.free)-1)]
+        self.mat[x][y] = 2
+        self.free = [(i, j) for i in range(4) for j in range(4) if not self.mat[i][j]]
+        self.score = 0
+
+
+    def update(self, direction):
+        if direction == DROITE:
+            for i in range(4):
+                for k in range(3, 0, -1):
+                    for j in range(k-1, -1, -1):
+                        if self.mat[i][k] == self.mat[i][j]:
+                            self.mat[i][k] *= 2
+                            self.mat[i][j] = 0
+                        elif self.mat[i][k] == 0:
+                            self.mat[i][k] = self.mat[i][j]
+                            self.mat[i][j] = 0
+        elif direction == GAUCHE:
+            for i in range(4):
+                for k in range(3):
+                    for j in range(k+1, 4):
+                        if self.mat[i][k] == self.mat[i][j]:
+                            self.mat[i][k] *= 2
+                            self.mat[i][j] = 0
+                        elif self.mat[i][k] == 0:
+                            self.mat[i][k] = self.mat[i][j]
+                            self.mat[i][j] = 0
+        elif direction == HAUT:
+            for i in range(4):
+                for k in range(3):
+                    for j in range(k+1, 4):
+                        if self.mat[k][i] == self.mat[j][i]:
+                            self.mat[k][i] *= 2
+                            self.mat[j][i] = 0
+                        elif self.mat[k][i] == 0:
+                            self.mat[k][i] = self.mat[j][i]
+                            self.mat[j][i] = 0
+        elif direction == BAS:
+            for i in range(4):
+                for k in range(3, 0, -1):
+                    for j in range(k-1, -1, -1):
+                        if self.mat[k][i] == self.mat[j][i]:
+                            self.mat[k][i] *= 2
+                            self.mat[j][i] = 0
+                        elif self.mat[k][i] == 0:
+                            self.mat[k][i] = self.mat[i][j]
+                            self.mat[j][i] = 0
+
+    def play(self, direction):
+        for i in range(4):
+            print(self.mat[i])
+        print()
+        self.update(direction)
+        x,y = self.free[randint(0, len(self.free)-1)]
+        self.mat[x][y] = 2
+        self.free = [(i, j) for i in range(4) for j in range(4) if not self.mat[i][j]]
+        for i in range(4):
+            print(self.mat[i])
+        print(self.free)
+        print('\n')
+
+g = Game()
+g.play(HAUT)
+g.play(HAUT)
+g.play(BAS)
+g.play(GAUCHE)
+g.play(DROITE)
+g.play(BAS)
