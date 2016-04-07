2ae734398a32425a4513b99f136773584cb56643
adrien Bodineau <adrien.bodineau@gmail.com>
2016-04-07 18:02:44 +0200

    update and print score

--- python/2048.py
+++ python/2048.py
@@ -22,9 +22,10 @@ class Game():
             for i in range(4):
                 for k in range(3, 0, -1):
                     for j in range(k-1, -1, -1):
-                        if self.mat[i][k] == self.mat[i][j]:
+                        if self.mat[i][k] != 0 and self.mat[i][k] == self.mat[i][j]:
                             self.mat[i][k] *= 2
                             self.mat[i][j] = 0
+                            self.score += 1
                         elif self.mat[i][k] == 0:
                             self.mat[i][k] = self.mat[i][j]
                             self.mat[i][j] = 0
@@ -32,9 +33,10 @@ class Game():
             for i in range(4):
                 for k in range(3):
                     for j in range(k+1, 4):
-                        if self.mat[i][k] == self.mat[i][j]:
+                        if self.mat[i][k] != 0 and self.mat[i][k] == self.mat[i][j]:
                             self.mat[i][k] *= 2
                             self.mat[i][j] = 0
+                            self.score += 1
                         elif self.mat[i][k] == 0:
                             self.mat[i][k] = self.mat[i][j]
                             self.mat[i][j] = 0
@@ -42,9 +44,10 @@ class Game():
             for i in range(4):
                 for k in range(3):
                     for j in range(k+1, 4):
-                        if self.mat[k][i] == self.mat[j][i]:
+                        if self.mat[k][i] != 0 and self.mat[k][i] == self.mat[j][i]:
                             self.mat[k][i] *= 2
                             self.mat[j][i] = 0
+                            self.score += 1
                         elif self.mat[k][i] == 0:
                             self.mat[k][i] = self.mat[j][i]
                             self.mat[j][i] = 0
@@ -52,9 +55,10 @@ class Game():
             for i in range(4):
                 for k in range(3, 0, -1):
                     for j in range(k-1, -1, -1):
-                        if self.mat[k][i] == self.mat[j][i]:
+                        if self.mat[k][i] != 0 and self.mat[k][i] == self.mat[j][i]:
                             self.mat[k][i] *= 2
                             self.mat[j][i] = 0
+                            self.score += 1
                         elif self.mat[k][i] == 0:
                             self.mat[k][i] = self.mat[i][j]
                             self.mat[j][i] = 0
@@ -70,6 +74,7 @@ class Game():
         for i in range(4):
             print(self.mat[i])
         print(self.free)
+        print(self.score)
         print('\n')
 
 g = Game()
